81b320c16058fc39290fe56ddca9083d20186c7a
Subhranil Mondal <hsuswiowkskow@gmail.com>
2025-05-05 11:31:10 +0530

    Update index.ts

--- src/components/WalletContext/index.ts
+++ src/components/WalletContext/index.ts
@@ -1,9 +1,8 @@
-// Import the necessary components
+
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 
 const projectId = "a1bda241f0519c5d662bd07e0705acaf";
 
-// Setup the Metadata
 const walletConnectMetadata = {
   name: "Ferever Memory",
   description: "dApp using Wallet Connect",
@@ -11,12 +10,12 @@ const walletConnectMetadata = {
   icons: ["https://my.dapp.domain/icon.svg"],
 };
 
-// Initialize the Configuration Element
+
 const walletConnectConfig = defaultConfig({
   metadata: walletConnectMetadata,
 });
 
-// Define the supported networks
+
 const supportedChains = [
   {
     chainId: 4021,
@@ -34,17 +33,17 @@ const supportedChains = [
   },
 ];
 
-// Define chain images for the network screen
+
 const walletConnectChainImages = {
   42: "https://my.dapp.domain/lyx_symbol.svg",
   4201: "https://my.dapp.domain/lyx_symbol.svg",
 };
 
-// Create the Web3 Modal Instance
+
 export const walletConnectInstance = createWeb3Modal({
   ethersConfig: walletConnectConfig,
   chains: supportedChains,
-  projectId, // Import the project ID from https://cloud.walletconnect.com
+  projectId,
   chainImages: walletConnectChainImages,
-  featuredWalletIds: ["NONE"], // OPTIONAL: Only show wallets that are installed by the user
+  featuredWalletIds: ["NONE"], 
 });
