6f94a474d3cf4ebbe7515f9abe26381f1305d45a
Subhranil Mondal <hsuswiowkskow@gmail.com>
2025-05-05 11:36:37 +0530

    Update upload.ts

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -12,13 +12,13 @@ export async function encryptFile(
   file: File,
   encryptionKey: Uint8Array
 ): Promise<Uint8Array> {
-  // Convert file to ArrayBuffer directly
+ 
   const fileBuffer = await file.arrayBuffer();
 
-  // Generate a random initialization vector (IV)
+ 
   const iv = crypto.getRandomValues(new Uint8Array(16));
 
-  // Import encryption key
+  
   const key = await crypto.subtle.importKey(
     "raw",
     encryptionKey,
@@ -27,14 +27,14 @@ export async function encryptFile(
     ["encrypt"]
   );
 
-  // Encrypt the file content directly
+  
   const encryptedData = await crypto.subtle.encrypt(
     { name: "AES-CBC", iv },
     key,
     fileBuffer
   );
 
-  // Concatenate IV with encrypted data
+ 
   return new Uint8Array([...iv, ...new Uint8Array(encryptedData)]);
 }
 
@@ -42,19 +42,15 @@ export async function decryptFile(
   encryptedData: Uint8Array,
   encryptionKey: Uint8Array
 ): Promise<Buffer> {
-  // Extract IV (Initialization Vector) from the encrypted data
+  
   const iv = encryptedData.slice(0, 16);
-
-  // Extract encrypted content (excluding IV)
   const encryptedContent = encryptedData.slice(16);
-
-  // Create a Decipheriv object using AES-CBC algorithm
   const decipher = createDecipheriv("aes-256-cbc", encryptionKey, iv);
 
-  // Perform decryption
+ 
   let decryptedData = decipher.update(encryptedContent);
   decryptedData = Buffer.concat([decryptedData, decipher.final()]);
 
-  // Return decrypted data as a Buffer
+ 
   return decryptedData;
 }
